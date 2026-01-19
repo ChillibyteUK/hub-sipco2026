ae930f2bb0b7ba3a21d6a5f717329cf83bcd6384
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-19 16:05:36 +0000

    nav

--- blocks/hub-icon-bar.php
+++ blocks/hub-icon-bar.php
@@ -10,7 +10,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <section class="icon-bar has-sipco-red-background-color has-white-color py-5">
 	<div class="container">
-		<div class="row justify-content-center">
+		<div class="row g-5 justify-content-center">
 			<div class="col-md-4 text-center fw-bold">
 				<?= wp_get_attachment_image( get_field( 'icon_1' ), 'full', false, array( 'class' => 'd-block mx-auto mb-3' ) ); ?>
 				<div class="w-constrained-xs mx-auto"><?= esc_html( get_field( 'text_1' ) ); ?></div>
