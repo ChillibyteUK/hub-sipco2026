484e34cba7e8e055e97d19a9add148f97fcbbd5a
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-22 10:05:19 +0000

    stat bar case

--- blocks/hub-share-info-bar.php
+++ blocks/hub-share-info-bar.php
@@ -12,15 +12,15 @@ defined( 'ABSPATH' ) || exit;
     <div class="container py-5">
         <div class="row g-3 justify-content-center text-center">
             <div class="col-md-4">
-                <h2>Share Price</h2>
+                <h2>Share price</h2>
                 <div class="share-info-bar__value"><?= wp_kses_post( get_field( 'share_price' ) ); ?></div>
             </div>
             <div class="col-md-4">
-                <h2>NAV per Share</h2>
+                <h2>NAV per share</h2>
                 <div class="share-info-bar__value"><?= wp_kses_post( get_field( 'nav_per_share' ) ); ?></div>
             </div>
             <div class="col-md-4">
-                <h2>NAV of Fund</h2>
+                <h2>NAV of fund</h2>
                 <div class="share-info-bar__value"><?= wp_kses_post( get_field( 'nav_of_fund' ) ); ?></div>
             </div>
         </div>
