ae20d3c5e7c14116bc04a6055dc210e2ddc3e8ad
ChillibyteUK <dan@chillibyte.co.uk>
2026-01-22 15:04:29 +0000

    accordion margin

--- blocks/hub-icon-accordion.php
+++ blocks/hub-icon-accordion.php
@@ -28,7 +28,7 @@ $accordion_id = 'accordion-' . uniqid();
 				$show      = '';
 				$expanded  = 'false';
 				?>
-			<div class="accordion-item mb-3" data-aos="fade-up" data-aos-delay="<?= esc_attr( 100 * ( $i + 1 ) ); ?>">
+			<div class="accordion-item" data-aos="fade-up" data-aos-delay="<?= esc_attr( 100 * ( $i + 1 ) ); ?>">
 				<h2 class="accordion-header" id="heading-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
 					<button class="accordion-button <?= esc_attr( $collapsed ); ?>" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>" aria-expanded="<?= esc_attr( $expanded ); ?>" aria-controls="collapse-<?= esc_attr( $accordion_id . '-' . $i ); ?>">
 						<?= wp_get_attachment_image( $it['icon'] ?? '' ); ?>
