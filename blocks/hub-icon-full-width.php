6e6d2f2611afb0a4e983bcbd30757ffca29c19f8
ChillibyteUK <dan@chillibyte.co.uk>
2026-03-20 10:24:58 +0000

    icon full width

--- blocks/hub-icon-full-width.php
+++ blocks/hub-icon-full-width.php
@@ -16,12 +16,27 @@ if ( empty( $classes ) ) {
 	$classes = '';
 }
 
+$presentation = get_field( 'presentation' );
+if ( 'Logo' === $presentation ) {
+	$col_one   = 'col-md-3';
+	$col_two   = 'col-md-9';
+	$img_class = 'full-width__logo';
+	$alignment = 'align-items-start';
+	$padding   = '';
+} else {
+	$col_one = 'col-md-1';
+	$col_two = 'col-md-11';
+	$img_class = 'full-width__icon my-auto';
+	$alignment = 'align-items-center';
+	$padding   = 'ps-4';
+}
+
 ?>
 <section class="full-width <?= esc_attr( $classes ); ?>">
     <div class="container <?= esc_attr( $bg ); ?>">
-		<div class="px-5" data-aos="fade-up">
-			<div class="row align-items-center">
-				<div class="col-md-2 mb-4 mb-md-0 ps-0 text-center">
+		<div data-aos="fade-up" class="<?= esc_attr( $padding ); ?>">
+			<div class="row <?= esc_attr( $alignment ); ?>">
+				<div class="<?= esc_attr( $col_one ); ?> mb-4 mb-md-0 ps-0 text-center">
 					<?php
 					if ( get_field( 'icon' ) ) {
 						echo wp_get_attachment_image(
@@ -29,14 +44,14 @@ if ( empty( $classes ) ) {
 							'full',
 							false,
 							array(
-								'class' => 'full-width__icon my-auto',
+								'class' => esc_attr( $img_class ),
 								'alt'   => esc_attr( get_field( 'title' ) . ' Icon' ),
 							)
 						);
 					}
 					?>
 				</div>
-				<div class="col-md-10 <?= ! get_field( 'title' ) ? 'no-title' : ''; ?>">
+				<div class="<?= esc_attr( $col_two ); ?> <?= ! get_field( 'title' ) ? 'no-title' : ''; ?>">
 					<?php
 					if ( get_field( 'title' ) ) {
 						?>
